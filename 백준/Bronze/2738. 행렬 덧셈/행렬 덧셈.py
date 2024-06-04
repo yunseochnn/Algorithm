e66bdbad61269f332e618a26eccb6dce55ea1f1d
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-06-04 12:03:37 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int,input().split())
 arr1 = [list(map(int,input().split())) for _ in range(n)]
 arr2 = [list(map(int,input().split())) for _ in range(n)]
