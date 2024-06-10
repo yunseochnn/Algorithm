151fed01371530575f20632d82fded3ee85c063a
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-06-10 09:26:24 +0900

    [level 2] Title: 구명보트, Time: 7.85 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,10 +1,12 @@
-from collections import deque
 def solution(people, limit):
+    people.sort()
+    start, end = 0, len(people)-1
     count = 0
-    people=deque(sorted(people))
-    while people :
-        person = people.pop()
-        if len(people)>0 and person+people[0]<=limit:
-            people.popleft()
+    while start <= end:
+        if (people[start] + people[end] <= limit):
+            start += 1
+            end -= 1
+        else:
+            end -=1
         count+=1
     return count
