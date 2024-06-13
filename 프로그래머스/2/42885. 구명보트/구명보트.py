df55f28cab6c85bd2523bdd4a6f65c84e7e712ea
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-06-13 16:06:47 +0900

    [level 2] Title: 구명보트, Time: 8.30 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,12 +1,14 @@
 def solution(people, limit):
     people.sort()
-    start, end = 0, len(people)-1
-    count = 0
-    while start <= end:
-        if (people[start] + people[end] <= limit):
-            start += 1
-            end -= 1
+    start = 0
+    end = len(people)-1
+    answer = 0
+    while start<=end:
+        if people[end]+people[start]<=limit:
+            answer+=1
+            end-=1
+            start+=1
         else:
-            end -=1
-        count+=1
-    return count
+            answer+=1
+            end-=1
+    return answer
