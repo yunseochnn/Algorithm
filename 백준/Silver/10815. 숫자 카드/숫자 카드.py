b16df65f37163cc67c7b2a7b1349e85edd10a5f7
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-07-03 17:46:23 +0900

    [Silver V] Title: 숫자 카드, Time: 596 ms, Memory: 125176 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr1 = set(map(int,input().split()))
+m = int(input())
+arr2 = list(map(int,input().split()))
+
+for i in arr2:
+    if i in arr1:
+        print(1,end = ' ')
+    else:
+        print(0,end = ' ')
