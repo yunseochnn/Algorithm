92f283b388c69e1df2b2de1dae586062177c8509
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-07-07 21:02:54 +0900

    [Silver IV] Title: 국영수, Time: 432 ms, Memory: 86716 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(input().split())
+arr.sort(key=lambda x: (-int(x[1]),int(x[2]),-int(x[3]), x[0]))
+for i in arr:
+    print(i[0])
