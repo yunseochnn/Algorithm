32704dee073424cdd2729bab21def9f5d0216b45
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-07-17 21:19:26 +0900

    [level 3] Title: 합승 택시 요금, Time: 2422.03 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,31 +1,26 @@
 import sys
 input = sys.stdin.readline
-import heapq
 INF = int(1e9)
+
 def solution(n, s, a, b, fares):
-    graph = [[] for _ in range(n+1)]
-    for i in fares:
-        x,y,z = i
-        graph[x].append((y,z))
-        graph[y].append((x,z))
-    def dijkstra(start):
-        distance = [INF]*(n+1)
-        q = []
-        heapq.heappush(q,(0,start))
-        distance[start] = 0
-        while q:
-            dist, now = heapq.heappop(q)
-            if dist > distance[now]:
-                continue
-            for i in graph[now]:
-                cost = dist + i[1]
-                if cost < distance[i[0]]:
-                    distance[i[0]] = cost
-                    heapq.heappush(q,(cost,i[0]))
-        return distance
+    graph = [[INF] * (n+1) for _ in range(n+1)]
+    
+    for i in range(1, n+1):
+        graph[i][i] = 0
+
+    for fare in fares:
+        i, j, k = fare
+        graph[i][j] = k
+        graph[j][i] = k
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
     arr = []
-    t1 = dijkstra(s)
-    for i in range(1,n+1):
-        result = dijkstra(i)
-        arr.append(result[a]+result[b]+t1[i])
+    for i in range(1, n+1):
+        cost = graph[s][i] + graph[i][a] + graph[i][b] # 노드 i를 거쳐가는 비용
+        arr.append(cost)
+    
     return min(arr)
