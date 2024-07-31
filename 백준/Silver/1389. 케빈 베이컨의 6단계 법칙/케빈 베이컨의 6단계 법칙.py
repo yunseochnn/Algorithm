b49b6971c0b38c6a2f71c4d1ddb032f0fbae5a90
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-07-31 14:44:16 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,23 +1,24 @@
+from collections import deque
+def bfs(start):
+    visited = [False for _ in range(n+1)]
+    num = [0]*(n+1)
+    q = deque([start])
+    visited[start] = True
+    while q:
+        t = q.popleft()
+        for i in graph[t]:
+            if not visited[i]:
+                num[i] = num[t]+1
+                q.append(i)
+                visited[i] = True
+    return sum(num)
 n,m = map(int,input().split())
-INF = int(1e9)
-graph = [[INF]*(n+1) for _ in range(n+1)]
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a==b:
-            graph[a][b] = 0
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
     x,y = map(int,input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
-min_val = INF
-idx = 0
+    graph[x].append(y)
+    graph[y].append(x)
+result = []
 for i in range(1,n+1):
-    result = sum(graph[i][1:])
-    if min_val > result:
-        min_val = result
-        idx = i
-print(idx)
+    result.append(bfs(i))
+print(result.index(min(result))+1)
