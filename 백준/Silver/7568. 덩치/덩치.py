d1467107a61a46ba457292946a42dee323490790
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-08-02 18:23:34 +0900

    [Silver V] Title: 덩치, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    x,y = map(int,input().split())
+    arr.append((x,y))
+for i in arr:
+    rank = 1
+    for j in arr:
+        if i[0]<j[0] and i[1]<j[1]:
+            rank += 1
+    print(rank,end = " ")
