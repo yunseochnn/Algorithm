7ff2fd55c849a37820c2a0afa338b5f36856c50a
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-08-09 19:27:06 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    arr = [i for i in range(1,n+1)]
+    for _ in range(k):
+        for j in range(1,n):
+            arr[j] += arr[j-1]
+    print(arr[-1])
