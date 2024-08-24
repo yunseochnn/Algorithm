acf024bb9662321c5192831447447f1b1a5a096e
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-08-24 19:33:38 +0900

    [Silver IV] Title: 스택, Time: 488 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,23 @@
+n = int(input())
+s=[]
+for _ in range(n):
+    command = input().split()
+    if command[0] == "push":
+        s.append(command[1])
+    elif command[0] == "pop":
+        if len(s) == 0:
+            print(-1)
+        else:
+            print(s.pop())
+    elif command[0] == "size":
+        print(len(s))
+    elif command[0] == "empty":
+        if len(s) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == "top":
+        if len(s) == 0:
+            print(-1)
+        else:
+            print(s[-1])
