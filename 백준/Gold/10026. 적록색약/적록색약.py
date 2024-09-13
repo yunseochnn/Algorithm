7589454d9e3176c6364ff4527059e032e54ce3a3
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-09-13 14:49:47 +0900

    [Gold V] Title: 적록색약, Time: 56 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,32 +1,28 @@
 import sys
+sys.setrecursionlimit(10000)
 input = sys.stdin.readline
-from collections import deque
-n = int(input())
+n = int(input().strip())
 graph=[]
 visited = [[0]*n for _ in range(n)]
 result = [0,0]
 for _ in range(n):
     graph.append(list(input()))
-
-def bfs(x,y):
-    q = deque()
-    q.append((x,y))
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def dfs(x,y):
     visited[x][y] = 1
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    while q:
-        x,y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == graph[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1
-                q.append((nx,ny))
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n:
+            if not visited[nx][ny]:
+                if graph[x][y] == graph[nx][ny]:
+                    dfs(nx,ny)
 
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
-            bfs(i,j)
+            dfs(i,j)
             result[0]+=1
 for i in range(n):
     for j in range(n):
@@ -36,6 +32,6 @@ visited = [[0]*n for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if not visited[i][j]:
-            bfs(i,j)
+            dfs(i,j)
             result[1]+=1
 print(*result)
