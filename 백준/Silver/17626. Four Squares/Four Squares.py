023ed2ed3903fbf1af919e81c907fc603b738716
yunseochnn <102135302+yunseochnn@users.noreply.github.com>
2024-09-18 21:53:33 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 113064 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0, 1]
+for i in range(2, n+1):
+    min_value = 4
+    for j in range(1, int(i ** 0.5) + 1):
+        min_value = min(min_value, dp[i-(j ** 2)])
+    dp.append(min_value + 1)
+print(dp[n])
